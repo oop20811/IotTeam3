c0425025309eaada5e984c859652e51bdb0d3711
oop20811 <oop20811@tukorea.ac.kr>
2025-01-20 13:29:15 +0900

    4번째 중간저장

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -4,8 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     {
       id: {
         type: DataTypes.INTEGER,
-        primaryKey: true,
         autoIncrement: true,
+        primaryKey: true,
       },
       slotNumber: {
         type: DataTypes.INTEGER,
@@ -22,14 +22,18 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       tableName: "products",
-      timestamps: true, // timestamps 활성화
+      timestamps: true,
     }
   );
+
   Product.associate = (models) => {
     Product.belongsTo(models.Slot, {
-      foreignKey: "slotNumber", // Product에서 외래키로 사용할 컬럼
-      targetKey: "slotNumber", // Slot의 참조 키
+      foreignKey: "slotNumber",
+      targetKey: "slotNumber",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
     });
   };
+
   return Product;
 };
